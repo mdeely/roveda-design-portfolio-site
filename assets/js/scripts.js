ce12b9300c47529de1efe54a3a1ab1d13f4cb69d
Marc Deely <marcdeely@Marcs-MacBook-Pro-2.local>
2019-01-20 23:33:41 -0600

    - Added behavoir for slideshow - Updated styling for slideshow - Updated scripts in head for slideshow

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,67 @@
 $( document ).ready(function() {
+
+	//////////////////////
+	// Gather elements //
+	/////////////////////
+
+	if ( $(".carousel__container").length > 0 ) {
+		var carouselPreviousClassName = "carousel__previous";
+		var carouselNextClassName = "carousel__next";
+		var $slider_controls = $(".carousel__next, .carousel__previous");
+		var slideClassName = "carousel__slide";
+		var slideCount = $("."+slideClassName).length;
+	}
+
+	////////////////////
+	// Bind to events //
+	////////////////////
+	$slider_controls.on('click', handleSlideshow );
+
+
+	///////////////
+	// Functions //
+	///////////////
+
+	function handleSlideshow(direction) {
+        direction.stopImmediatePropagation();
+		direction.preventDefault();
+
+		var activeSlide = getActiveSlide();
+		var requestedSlide = null;
+		var slideshowNumber = $(activeSlide).data("slideshow-index");
+
+		if ( $(direction.target).hasClass(carouselPreviousClassName) ) {
+			requestedSlide = slideshowNumber-1;
+
+			if ( requestedSlide == 0 ) {
+				requestedSlide = slideCount;
+			}
+		};
+
+		if ( $(direction.target).hasClass(carouselNextClassName) ) {
+			requestedSlide = slideshowNumber+1;
+
+			if ( requestedSlide > slideCount ) {
+				requestedSlide = 1;
+			};
+
+		};
+
+		updateSlideshow(requestedSlide, activeSlide)
+	}
+
+	function getActiveSlide() {
+		var activeSlide = $("."+slideClassName+".active");
+		return activeSlide;
+	}
+
+	function updateSlideshow(requestedSlide, activeSlide) {
+		var requestedSlide = $("."+slideClassName+"[data-slideshow-index="+requestedSlide+"]");
+
+		$(activeSlide).removeClass("active");
+		$(requestedSlide).addClass("active");
+	}
+
 	// if next is clicked
 	// 	go to .classname plus one
 	// 		if that doesn't exist, go back to first
